0f478efcd9823a3cac6475abf5e0f3b95a38482b
teo2323 <cotofan.teodor@gmail.com>
2026-06-16 14:40:14 +0300

    creare api furnizare info trenuri Resolves #27

--- backend/src/main/java/com/smartrail/backend/controller/TrenController.java
+++ backend/src/main/java/com/smartrail/backend/controller/TrenController.java
@@ -3,23 +3,33 @@ package com.smartrail.backend.controller;
 import com.smartrail.backend.model.Tren;
 import com.smartrail.backend.repository.TrenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/trenuri")
+@CrossOrigin(origins = "*")
 public class TrenController {
-
 
     @Autowired
     private TrenRepository trenRepository;
 
-    @GetMapping("/search")
+    @GetMapping
     public List<Tren> getTrenuri() {
-
         return trenRepository.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<Tren> searchTrenuri() {
+        return trenRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tren> getTrenById(@PathVariable String id) {
+        return trenRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
